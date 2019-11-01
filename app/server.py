70a1d3c6aec6e5c2d160e1d00fcefb6c9a5d1933
vk9999 <venkatvmware99@gmail.com>
2019-11-01 00:34:27 -0500

    4

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/'
+export_file_url = 'https://www.dropbox.com/s/vrtda03xprl5g3s/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['CannabisFlower', 'CannabisPlant', 'HempFlower', 'HempPlant']
@@ -61,7 +61,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': learn.predict(img)[0].obj})
+    return JSONResponse({'result': str(prediction)})
 
 
 if __name__ == '__main__':
